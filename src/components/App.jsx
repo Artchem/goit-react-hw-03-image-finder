2bba5eed75552ded99ed5bb4be64d1d26013abb9
Artchem <chemerys.artem@gmail.com>
2023-09-24 00:15:53 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Modal } from './Modal/Modal';
+
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(prevState => ({ showModal: !prevState.showModal }));
+  };
+
+  render() {
+    return <div>{this.state.showModal && <Modal />}</div>;
+  }
+}
